a154838c9703ab10c3bd29af1a6b3a3b18cd4171
KKural <122997366+KKural@users.noreply.github.com>
2025-03-04 11:45:13 +0100

    Update urls.py

--- cric_core/urls.py
+++ cric_core/urls.py
@@ -1,7 +1,8 @@
-from django.contrib.auth import views as auth_views
+from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('admin/', admin.site.urls),  # Admin panel
+    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),  # Login page
 ]
